3d26e0e0ddb441b9751a62b45b4cae051c617605
julfalqu <julfalqu@publicisgroupe.net>
2023-07-27 17:32:02 -0500

    added home page and profile page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Blob from '../public/blobanimation.svg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola
+      <div className='relative'>
+
+      <div className="absolute top-[20vh]">
+        <p className="text-6xl">Bienvenido a</p>
+        <h1 className="text-8xl font-extrabold">EVNT TRCKR</h1>
+        <p className="text-6xl">nunca te pierdas un evento</p>
+      </div>
+      <Image
+      priority
+      src={Blob}
+      alt="Follow us on Twitter "
+      width={800}
+      />
+      </div>
     </main>
   );
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,35 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
 const Profile = () => {
   const { data: session, status } = useSession();
+  const [userData, setuserData] = useState<undefined | any>(undefined);
   console.log(session, status);
-  return <div>profile</div>;
+  useEffect(() => {
+    setuserData(session?.user);
+  }, [session]);
+
+  return (
+    <>
+      {userData && (
+        <>
+          <div>Welcome,{userData.display_name}</div>
+          <img
+            className="rounded-full"
+            src={userData.profilePicture}
+            alt="profile picture"
+            width={100}
+            height={100}
+          />
+          <div className="flex flex-row">
+            <div>My events</div>
+            <div>Attending events</div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Profile;
