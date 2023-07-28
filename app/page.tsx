d5ca40ab938743386385e6120842ee146bf6ddf1
Randy Aguirre <140548356+ranaguir97@users.noreply.github.com>
2023-07-28 06:37:28 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,7 @@ export default function Home() {
       <div className="absolute md:top-[250px] top-[150px]">
         <p className="text-6xl">Welcome to</p>
         <h1 className="text-8xl font-extrabold">EVNT TRCKR</h1>
-<<<<<<< HEAD
         <p className="text-6xl">never miss out on an event</p>
-=======
-        <p className="text-6xl">nunca te pierdas un evento.</p>
->>>>>>> 82e1612549b158f27c68dff97a3c35a1c1857c94
       </div>
       <Image
       priority
